58cb002d1aedb1e63c29fc66088692a5201d83cc
Page Tailoring <bartek@pagetailoring.com>
2025-04-16 16:59:28 +0200

    feat: implement login/logout functionality with custom button components and enhance secret content display

--- components/TheSecretContent.vue
+++ components/TheSecretContent.vue
@@ -1,4 +1,5 @@
 <template>
   <p>You're logged in</p>
   <p>This is The Secret Content</p>
+  <AuthLogoutButton />
 </template>
---
+++ components/Ui/Button.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+const { type = 'button' } = defineProps<{
+  type?: 'button' | 'submit' | 'reset' | undefined
+}>()
+</script>
+
+<template>
+  <button style="padding: 0.5em; margin-block: 1em" :type="type"><slot /></button>
+</template>
