2d1aee41afd8f7e1c15a9ba4923caac0a276f240
Page Tailoring <bartek@pagetailoring.com>
2025-04-16 14:35:05 +0200

    stylling, web vitals fix. Chore: update README for clarity, enhance app.vue structure, and refine nuxt.config.ts configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,22 @@
 export default defineNuxtConfig({
   modules: ['@nuxt/eslint'],
   devtools: { enabled: true },
+
+  app: {
+    head: {
+      htmlAttrs: { lang: 'en', style: 'color-scheme: dark;' },
+      title: 'Nuxt Auth micro',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width,initial-scale=1' },
+        { name: 'description', content: 'A project focused on analyzing bundle size with different tools.' },
+      ],
+    },
+  },
   compatibilityDate: '2024-11-01',
   typescript: {
     typeCheck: true,
     strict: true,
   },
-
-  eslint: {
-    config: { stylistic: true },
-  },
+  eslint: { config: { stylistic: true } },
 })
